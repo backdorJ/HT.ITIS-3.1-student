5d525a7243f328a75db7ec1f7678677e0b94c2be
dammirka <cdbacjdorz@yandex.ru>
2025-01-21 18:39:24 +0300

    fix tests

--- Dotnet.Homeworks.Features/UserManagement/Commands/DeleteUserByAdmin/DeleteUserByAdminCommandHandler.cs
+++ Dotnet.Homeworks.Features/UserManagement/Commands/DeleteUserByAdmin/DeleteUserByAdminCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Dotnet.Homeworks.Infrastructure.Cqrs.Commands;
+﻿using Dotnet.Homeworks.Domain.Abstractions.Repositories;
+using Dotnet.Homeworks.Infrastructure.Cqrs.Commands;
 using Dotnet.Homeworks.Infrastructure.UnitOfWork;
 using Dotnet.Homeworks.Shared.Dto;
 
@@ -7,10 +8,12 @@ namespace Dotnet.Homeworks.Features.UserManagement.Commands.DeleteUserByAdmin;
 public class DeleteUserByAdminCommandHandler : ICommandHandler<DeleteUserByAdminCommand> 
 {
     private readonly IUnitOfWork _unitOfWork;
-
-    public DeleteUserByAdminCommandHandler(IUnitOfWork unitOfWork)
+    private readonly IUserRepository _userRepository;
+    
+    public DeleteUserByAdminCommandHandler(IUnitOfWork unitOfWork, IUserRepository userRepository)
     {
         _unitOfWork = unitOfWork;
+        _userRepository = userRepository;
     }
 
     public async Task<Result> Handle(DeleteUserByAdminCommand request, CancellationToken cancellationToken)
@@ -21,12 +24,12 @@ public class DeleteUserByAdminCommandHandler : ICommandHandler<DeleteUserByAdmin
 
         try
         {
-            // var isExist = await _unitOfWork.UserRepository.GetUserByGuidAsync(request.Guid, cancellationToken);
-            //
-            // if (isExist == null)
-            //     return new Result(false, $"User {request.Guid} does not exist.");
+            var isExist = await _userRepository.GetUserByGuidAsync(request.Guid, cancellationToken);
             
-            await userRepo.DeleteUserByGuidAsync(request.Guid, cancellationToken);
+            if (isExist == null)
+                return new Result(false, $"User {request.Guid} does not exist.");
+            
+            await _userRepository.DeleteUserByGuidAsync(request.Guid, cancellationToken);
             await _unitOfWork.SaveChangesAsync(cancellationToken);
             
             return new Result(true);
