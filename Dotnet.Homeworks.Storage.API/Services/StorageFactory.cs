d2e2425381740a3293fa21e3f3321facf282d5ee
dammirka <cdbacjdorz@yandex.ru>
2025-02-01 12:08:20 +0300

    Homework-8 Добавлена поддержка Minio

--- Dotnet.Homeworks.Storage.API/Services/StorageFactory.cs
+++ Dotnet.Homeworks.Storage.API/Services/StorageFactory.cs
@@ -1,13 +1,35 @@
 ﻿using Dotnet.Homeworks.Storage.API.Dto.Internal;
+using Minio;
 
 namespace Dotnet.Homeworks.Storage.API.Services;
 
 public class StorageFactory : IStorageFactory
 {
-    public Task<IStorage<Image>> CreateImageStorageWithinBucketAsync(string bucketName)
+    private readonly IMinioClient _client;
+
+    public StorageFactory(IMinioClient client)
     {
-        // TODO: implement creation of IImageStorage with the given bucketName
-        // e.g. each storage should work only within its bucket (but still may copy items to another bucket)
-        throw new NotImplementedException();
+        _client = client;
+    }
+
+    public async Task<IStorage<Image>> CreateImageStorageWithinBucketAsync(string bucketName)
+    {
+        await MakeBucketIfNotExistsAsync(bucketName);
+
+        return new ImageStorage(_client, bucketName);
+    }
+
+    private async Task MakeBucketIfNotExistsAsync(string bucket, CancellationToken cancellationToken = default)
+    {
+        var isBucketExists = await _client.BucketExistsAsync(new BucketExistsArgs()
+            .WithBucket(bucket), cancellationToken);
+
+        if (isBucketExists)
+        {
+            return;
+        }
+
+        await _client.MakeBucketAsync(new MakeBucketArgs()
+            .WithBucket(bucket), cancellationToken);
     }
 }
